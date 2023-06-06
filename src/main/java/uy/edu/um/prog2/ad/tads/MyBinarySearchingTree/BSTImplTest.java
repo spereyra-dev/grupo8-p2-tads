f2800c5c0f0d0b682ac6ee07c204e564c3c293b8
Alexia Gerstein <alexiagerstein8@gmail.com>
2023-06-06 14:05:44 -0300

    Added binary search tree

---
+++ src/main/java/uy/edu/um/prog2/ad/tads/MyBinarySearchingTree/BSTImplTest.java
@@ -0,0 +1,40 @@
+package uy.edu.um.prog2.ad.tads.MyBinarySearchingTree;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.junit.jupiter.api.Test;
+
+public class BSTImplTest {
+    MyBinarySearchingTree<Integer,String> bst = new BinarySearchingTreeImpl<>();
+
+    @Test
+    void findTest(){
+        //Insertamos nodos en el árbol binario de búsqueda
+        bst.insert(6,"A"); //Insertar nodo raíz
+        bst.insert(8,"B"); //Insertar nodo a la derecha de el nodo raíz
+        bst.insert(2,"C"); //Insertar nodo a la izquierda de el nodo raíz
+
+        //Realizar pruebas y afirmaciones
+        assert bst.find(6).equals("A");
+        assertNotEquals("B", bst.find(6));
+        assert bst.find(2).equals("C");
+    }
+
+    @Test
+    void deleteTest(){
+        //Insertamos nodos en el árbol binario de búsqueda
+        bst.insert(6,"A"); //Insertar nodo raíz
+        bst.insert(8,"B"); //Insertar nodo a la derecha de el nodo raíz
+        bst.insert(2,"C"); //Insertar nodo a la izquierda de el nodo raíz
+        bst.insert(1,"D"); //Insertar nodo a la izquierda de 2
+        bst.insert(4,"E"); //Insertar nodo a la derecha de 2
+        bst.insert(3,"F"); //Insertar nodo a la izquierda de 4
+
+        //Se elimina el nodo 4, quedando el nodo 3 como hijo izquierdo del nodo 2
+        bst.delete(4);
+
+        //Realizar pruebas y afirmaciones
+        assert bst.find(4)==null;
+
+
+    }
+
+}
